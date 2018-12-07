23c9bfcb0fd665ce42bb874ab02b37ff8b6db8f7
kazumasa <kazumasa.matsumoto62@gmail.com>
2018-12-07 14:05:30 +0900

    Step7修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,30 +1,42 @@
-require("./bootstrap");
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
 
 // Vue
-import Vue from "vue";
+import Vue from 'vue'
+
 
 // Vuetify
-import Vuetify from "vuetify";
-import colors from "vuetify/es5/util/colors";
-
+import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
+/*
 Vue.use(Vuetify, {
-    theme: {
-        primary: colors.amber.base,
-        secondary: colors.blue.base,
-        accent: colors.amber.base
-    }
+  theme: {
+    primary: colors.amber.base,
+    secondary: colors.blue.base,
+    accent: colors.indigo.base,
+  }
 });
-import "vuetify/dist/vuetify.min.css";
-import "material-design-icons-iconfont/dist/material-design-icons.css";
+*/
+Vue.use(Vuetify)
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
 
 // Vue-Router
-import router from "./router";
+import router from './router'
+
 
 // moment
 window.moment = require('moment')
 
+
 // Main app
 const app = new Vue({
-    el: "#app",
-    router
+    el: '#app',
+    router,
 });
