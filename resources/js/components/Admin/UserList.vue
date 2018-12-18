379d35f62d66d104665ba3816641cf299bdb77be
kazumasa <kazumasa.matsumoto62@gmail.com>
2018-12-18 21:54:17 +0900

    Userlistを追加ページのみ機能は後日追加

---
+++ resources/js/components/Admin/UserList.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>UserList Component.</h1>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+    }),
+
+    props: {
+    },
+
+    created() {
+      console.log('UserList Component created.')
+      this.initialize()
+    },
+
+    mounted() {
+      console.log('UserList Component mounted.')
+    },
+
+    methods: {
+      initialize: function() {
+      },
+    },
+
+  }
+</script>
